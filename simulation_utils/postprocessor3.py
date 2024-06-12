be3ee5e3ed46007e2e60551c6c9a62d4d68c3670
RitioL <68575748+RitioL@users.noreply.github.com>
2024-06-12 15:14:50 +0800

    Update postprocessor3.py

--- simulation_utils/postprocessor3.py
+++ simulation_utils/postprocessor3.py
@@ -6,8 +6,8 @@ frame = 19
 
 # Execute step 1
 # Extract selected frames' PHILSM information from Odb file
-subprocess.run(["python", "simulation_utils\postprocessor5-step1.py", wp_name, str(frame)])
+subprocess.run(["python", "simulation_utils\postprocessor3-step1.py", wp_name, str(frame)])
 
 # Execute step 2
 # Draw EBSD and crack images
-subprocess.run(["python", "simulation_utils\postprocessor5-step2.py", wp_name, str(frame)])
+subprocess.run(["python", "simulation_utils\postprocessor3-step2.py", wp_name, str(frame)])
